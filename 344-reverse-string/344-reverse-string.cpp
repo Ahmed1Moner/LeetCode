e02f11f584c1c923cb4448c6e7e8d59011a77fac
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-06-03 01:35:43 +0200

    Time: 45 ms (13.76%), Space: 23 MB (93.72%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        /*
+        traverse over the whole string with two pointers, acutally they're two indecies. one from the beggining, the other from the end.
+        swap each letter indicated with his pointer with the second one until reaching the mid letter.
+        */
+        
+        //initialize the two pointers
+        int ptr1=0, ptr2=s.size()-1;
+        
+        //loop until the second pointer be bigger than the first one
+        while(ptr1<ptr2){
+            
+            //if the two pointers meet, skip this letter -> assuming that it swapped with himself.
+            if(ptr1==ptr2){
+                continue;
+            }
+            //swap the two letters
+            else{
+                int temp=s[ptr1];
+                s[ptr1]=s[ptr2];
+                s[ptr2]=temp;
+            }
+            //move the pointers
+            ptr1++,ptr2--;
+        }
+        return;
+    }
+};
