b9083a2d569234b37caa8ddef0992831bf0e7c94
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-06-03 13:35:32 +0200

    Time: 4 ms (29.86%), Space: 6.4 MB (10.59%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-
-  for(int i=0;i<s.length();i++){
-    if(st.empty()){st.push(s[i]);}
-      
-    else if((s[i]==')'&&st.top()=='(') || (s[i]==']'&&st.top()=='[') || (s[i]=='}'&&st.top()=='{')){
-      st.pop();
-    }
-    else
-        {
-            st.push(s[i]);
-        }
-    
-  }
         
-    if(st.empty()){ return true;}
-    else{return false;}
+        //initialize a stack from character type with name s1
+        stack<char> s1;
+        
+        //loop over the whole string
+        for(int i=0;i<s.length();i++){
+            
+            //if the stack is empty, add the parenthese to it
+            if(s1.empty())
+                s1.push(s[i]);
+            
+            //the entered parenthese must be from the same type of the one after it & in the correct order, the top from the same type
+            else if(s[i]==')'&&s1.top()=='('||s[i]=='}'&&s1.top()=='{'||s[i]==']'&&s1.top()=='[')
+                s1.pop();
+            
+            //if not, push again the next one
+            else
+                s1.push(s[i]);
+            
+        }
+        
+        //for a valid parentheses, the stack must be empty
+        if(s1.empty())
+            return true;
+        
+        else
+            return false;
+        
     }
 };
