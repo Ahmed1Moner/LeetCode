4104d22f77a14d70f1423ceeaea57b16fdef79a3
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-06-04 22:06:45 +0200

    Time: 311 ms (14.68%), Space: 70.7 MB (21.85%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -1,53 +1,64 @@
 class Solution {
 public:
-       
-        
-        void conquer(vector<int>& nums, int left, int mid, int right){
-            int size=right-left+1;
-            int *nums2=new int[size];
-            int i=left,j=mid+1,k=0;
-            
-            while(i<=mid&&j<=right){
-                if(nums[i]<nums[j]){
-                    nums2[k]=nums[i];
-                    i++;
-                }
-                else{
-                    nums2[k]=nums[j];
-                    j++;
-                }
-                k++;
-            }
-            
-            while(i<=mid){
+    
+   //merge sort -> worst: O(n*logn), best: O(n*logn), stable, out-place
+    
+    //conquer/collect array
+    void conquer(vector<int>& nums, int left, int mid, int right){
+        //determine the size of the array
+        int size=right-left+1;
+        //initialize a dynamic-allocated array
+        int *nums2=new int[size];
+        //initialize 3 pointers, one for each sub-array
+        int i=left,j=mid+1,k=0;
+
+        //collection time
+        //conquer the array in descending order
+        while(i<=mid&&j<=right){
+            if(nums[i]<nums[j]){
                 nums2[k]=nums[i];
                 i++;
-                k++;
             }
-            while(j<=right){
+            else{
                 nums2[k]=nums[j];
                 j++;
-                k++;
             }
-            
-            for(int i=0;i<size;i++){
-                nums[left+i]=nums2[i];
-            }
-            delete [] nums2;
+            k++;
         }
-        
-        void divide(vector<int>& nums, int left, int right){
-            if(left<right){
-                int mid=left+(right-left)/2;
-                divide(nums, left, mid);
-                divide(nums, mid+1, right);
-                conquer(nums, left, mid, right);
-            }
+
+        while(i<=mid){
+            nums2[k]=nums[i];
+            i++;
+            k++;
         }
-    
+        while(j<=right){
+            nums2[k]=nums[j];
+            j++;
+            k++;
+        }
+
+        //move every element from the new array to the old one
+        for(int i=0;i<size;i++){
+            nums[left+i]=nums2[i];
+        }
+        //delete the new array
+        delete [] nums2;
+    }
+
+    //divide array
+    void divide(vector<int>& nums, int left, int right){
+        //base case: do until reaching only one element
+        if(left<right){
+            int mid=left+(right-left)/2;
+            divide(nums, left, mid);
+            divide(nums, mid+1, right);
+            conquer(nums, left, mid, right);
+        }
+    }
+
     vector<int> sortArray(vector<int>& nums) {
-        int size=nums.size();
-       divide(nums, 0, size-1);
+        //calling the function divide, which contains the conquer function, too
+        divide(nums, 0, nums.size()-1);
         return nums;
     }
 };
