eb7446b75d57ddf6ee468b9dfefeafb839a29082
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-06-06 12:13:56 +0200

    Time: 36 ms (71.35%), Space: 9.6 MB (76.59%) - LeetHub

--- 147-insertion-sort-list/147-insertion-sort-list.cpp
+++ 147-insertion-sort-list/147-insertion-sort-list.cpp
@@ -8,23 +8,40 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
-class Solution{
-    public:
-    ListNode* insertionSortList(ListNode *head){
-        //loop over the whole list
-        for(auto i=head;i;i=i->next){
-            //loop until the ending of the first loop
-            for(auto j=head;j!=i;j=j->next){
-                //if the value of second iteration is smaller than the first one, swap the values
-                if(j->val>i->val){
-                    auto temp=j->val;
-                    j->val=i->val;
-                    i->val=temp;
-                }
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        
+        //creating a new list with the minimum value possible & its next is the head
+        ListNode *dummy=new ListNode(INT_MIN, head);
+        //temp pointer points on the next of the head
+        ListNode *temp=head->next;
+        //separate the two nodes, heed & dummy
+        head->next=NULL;
+        
+        //until reaching the null in the first list, the original one
+        while(temp){
+            //always initialize two pointers, prev points on dummy list & curr points to its next
+            ListNode *prev=dummy;
+            ListNode *curr=prev->next;
+            
+            //until reaching the null in the second list, dummy
+            
+            //comparing the values of the curr & temp, the heads of the two lists
+            
+            //if the head value is less than the temp one, it's ok, move to the next node
+            while(curr&&curr->val<temp->val){
+                prev=curr;
+                curr=curr->next;  
             }
+            //if not, if the head value is more than the temp one, wee need to move the temp node between them, in the right position 
+            prev->next=temp;
+            ListNode *next=temp->next;
+            temp->next=curr;
+            temp=next;
         }
-        return head;
+        //return the next of the new list, ignoring the first element which is added by us
+        return dummy->next;
+        
     }
 };
-
