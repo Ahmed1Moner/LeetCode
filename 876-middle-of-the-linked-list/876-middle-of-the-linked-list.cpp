ac01c75d1d04c03ae35445dd260ab5ad27088736
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-06-07 00:37:51 +0200

    Time: 0 ms (100.00%), Space: 7.1 MB (22.51%) - LeetHub

---
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        //initialize a pointer points on the head, the size of the list with zero
+        ListNode *traptr=head;
+        int size=0;
+        
+        //if there is no elements || only one element, return it
+        if(!head||!head->next){
+            return head;
+        }
+        
+        //loop over the whole list to get the number of the nodes
+        while(traptr){
+            size++;
+            traptr=traptr->next;
+        }
+        //make the pointer points back on the head
+        traptr=head;
+        
+        //move with the pointer until reaching the middle of the list
+        for(int i=1;i<(size/2);i++){
+            traptr=traptr->next;
+        }
+        //return the next of the pointer, as it's required to return the one after the middle
+        return traptr->next;
+    }
+};
