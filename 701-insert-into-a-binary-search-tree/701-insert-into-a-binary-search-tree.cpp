6a82c1d9f28556e5ac239eacb30aff498c552e18
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-06-12 22:51:37 +0200

    Time: 107 ms (68.53%), Space: 57 MB (49.63%) - LeetHub

---
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        //base case-> if it reaches the desired position, create a new node with the given data
+        if(!root){
+            TreeNode* newNode=new TreeNode;
+            newNode->left=NULL;
+            newNode->right=NULL;
+            newNode->val=val;
+            return newNode;
+        }
+        //if the given element is smaller than the current node-> ignore the values bigger than it
+        //recursivelly, search in the first smaller elements
+        if(val < root->val){
+            root->left=insertIntoBST(root->left, val);
+        }
+        //same algorithm as the above one
+        else if(val > root->val){
+            root->right=insertIntoBST(root->right, val);
+        }
+        //return the whole subtree
+        return root;
+    }
+};
