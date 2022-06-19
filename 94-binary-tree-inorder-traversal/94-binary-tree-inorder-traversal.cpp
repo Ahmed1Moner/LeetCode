58ff356367717205935d63eb552df5cf2e1396f7
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-06-19 10:17:59 +0200

    Time: 3 ms (58.35%), Space: 8.3 MB (75.49%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -11,20 +11,38 @@
  */
 class Solution {
 public:
-    //initialize a new empty vector with name result
-    vector<int> res={};
     vector<int> inorderTraversal(TreeNode* root) {
+        //if the tree's empty-> return null
+        if(!root){
+            return {};
+        }
         
-        //base case-> if the tree's empty, return the empty vector
-        if(!root){return res;}
-        
-        //recursive cases-> call the fuction with its left sub-tree
-        inorderTraversal(root->left);
-        //push the root node value
-        res.push_back(root->val);
-        //recursivelly, call the fuction with its right sub-tree
-        inorderTraversal(root->right);
-        
+        // & a stack to push the elements with the order I need in a vetor
+        stack<TreeNode*> st;
+        //initialize a vector to store in the ordered elements
+        vector<int> res;
+
+        //loop until finishing the tree elements & the stack isn't empty
+        while(root || !st.empty()){
+            //if I have a root node-> push it in the stack & move to the left sub-tree
+            //the case that stops this condition when the root points on null after the last left node
+            if(root){
+                st.push(root);
+                root=root->left;
+            }
+            //if I don't have a root node-> put the current node value in the vector
+            // & pop the current elmenet & move to the right sub-tree
+            else{
+                //since that it points on null to stop the above condition-> return it back to the topper element in the stack
+                root=st.top();
+                //add the current root value to the vector
+                res.push_back(root->val);
+                //delete the current added element from the stack
+                st.pop();
+                //move to the right sub-tree
+                root=root->right;
+            }
+        }
         //return the vector elements
         return res;
     }
