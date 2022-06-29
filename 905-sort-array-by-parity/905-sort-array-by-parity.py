f34aed60725f9af4f0461c647c70267b736ce499
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-06-29 04:54:14 +0200

    Time: 79 ms (95.56%), Space: 14.8 MB (17.49%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.py
+++ 905-sort-array-by-parity/905-sort-array-by-parity.py
@@ -1,10 +1,29 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        #using functional programming
+        #using iteration
         
-        #generate the even numbers after filtering the nums list's elements
-        even=list(filter(lambda x:x%2==0,nums))
-        #generate the odd numbers after filtering the nums list's elements
-        odd=list(filter(lambda x:x%2!=0,nums))
-        #return a list of even numbers first, then the odd ones
+        #initialize a two lists, one for even numbers & one for odd ones
+        even=[]
+        odd=[]
+        
+        #loop over the whole list numbers
+        for i in nums:
+            #if the number is even, add it to the even list
+            if(i%2==0):
+                even.append(i)
+            #if the number is odd, add it to the odd list
+            else:
+                odd.append(i)
+                
+        #return a list of even numbers first, then the odd ones        
         return even+odd
+    
+    
+#     #using functional programming
+        
+#         #generate the even numbers after filtering the nums list's elements
+#         even=list(filter(lambda x:x%2==0,nums))
+#         #generate the odd numbers after filtering the nums list's elements
+#         odd=list(filter(lambda x:x%2!=0,nums))
+#         #return a list of even numbers first, then the odd ones
+#         return even+odd
