978596398adc20c07874ca15ae89576aaf316cfe
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-07-13 18:22:34 +0200

    Time: 55 ms (69.76%), Space: 28.2 MB (98.29%) - LeetHub

--- 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.cpp
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.cpp
@@ -3,6 +3,31 @@ public:
     int minMoves(vector<int>& nums) {
         //initialize a counter to count the number of minimum moves
         int counter=0;
+        //initialze a variable with the maximum integer value
+        int m=INT_MAX;
+        
+        //loop over the elements to get the minimum one
+        for(int j=0;j<nums.size();j++)
+            //compare each element with the minimum value-> assign the min one
+            if(nums[j]<m)
+                m=nums[j];
+
+        //loop over the array elements
+        for(int num:nums)
+            //subtract the current num from the minimum number & add it to the counter
+            counter+=(num-m);
+
+        //return the counter value
+        return counter;
+    }
+};
+
+/*
+class Solution {
+public:
+    int minMoves(vector<int>& nums) {
+        //initialize a counter to count the number of minimum moves
+        int counter=0;
         //sort the input array elements using the built-in function
         sort(nums.begin(), nums.end());
 
@@ -15,3 +40,4 @@ public:
         return counter;
     }
 };
+*/
