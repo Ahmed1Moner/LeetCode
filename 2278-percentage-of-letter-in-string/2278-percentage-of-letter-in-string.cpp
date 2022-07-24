3bbbd16bd8f1baf8fbdf8823099bd30bdfbb846a
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-07-24 22:57:57 +0200

    Time: 0 ms (100.00%), Space: 6.1 MB (43.99%) - LeetHub

--- 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.cpp
+++ 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     int percentageLetter(string s, char letter) {
-        //initialize a counter
-        int counter=0;
-        
-        //loop over the array letters
-        for(char let:s)
-            //if the current letter is the same as the target one
-            if(let==letter)
-                //add the counter by one
-                counter++;
-        
-        //retun the percentage of its occurence
-        return ((double)counter/(double)s.size())*100;
+        return ((double)count(s.begin(),s.end(),letter)/(double)s.size())*100;
     }
 };
