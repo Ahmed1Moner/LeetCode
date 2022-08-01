3e91ec4479d191bb14ccea1382a0dc91910d0457
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-08-01 14:01:20 +0200

    Time: 75 ms (31.36%), Space: 9.8 MB (46.76%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.cpp
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -2,18 +2,37 @@ class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
         
-        //loop over the whole array elements
-        for(int i=0;i<arr.size();i++)
+        //loop over the whole array elemenst
+        for(int i=0;i<arr.size();i++){
             //if the current one is 0
             if(arr[i]==0){
-                //shift elements from the last to the current element
-                for(int j=arr.size()-1;j!=i;j--)
-                    arr[j]=arr[j-1];
-                    
+                //remove one element from the last
+                arr.pop_back();
+                //insert 0 in the next position after this 0
+                arr.insert(arr.begin()+i,0);
                 //move an extra step, skip the new zero element
                 i++;
             }
+        }
     }
 };
+
+// class Solution {
+// public:
+//     void duplicateZeros(vector<int>& arr) {
+        
+//         //loop over the whole array elements
+//         for(int i=0;i<arr.size();i++)
+//             //if the current one is 0
+//             if(arr[i]==0){
+//                 //shift elements from the last to the current element
+//                 for(int j=arr.size()-1;j!=i;j--)
+//                     arr[j]=arr[j-1];
+                    
+//                 //move an extra step, skip the new zero element
+//                 i++;
+//             }
+//     }
+// };
 
 
