685edc9e69c755dc584af7ba39dedb54fccce82d
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-08-15 04:59:45 +0200

    Time: 5 ms (47.13%), Space: 12.6 MB (66.22%) - LeetHub

--- 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.cpp
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.cpp
@@ -14,9 +14,7 @@ public:
     bool checkTree(TreeNode* root) {
         
         //check if the left & the right values equals the main root value
-        if((root->left->val + root->right->val) == root->val)
-            return true;
-        else
-            return false;
+        return ((root->left->val + root->right->val) == root->val);
+        
     }
 };
