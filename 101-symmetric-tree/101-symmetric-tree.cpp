4d00fdb60610a3527a237b7d9ecec3ab8667f7b0
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-08-15 07:12:02 +0200

    Time: 7 ms (71.07%), Space: 16.6 MB (9.60%) - LeetHub

---
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        
+        //initialize two stacks from the tree type
+        stack<TreeNode *> stp1, stp2;
+        //push the left node in the first stack & the right in the second one
+        stp1.push(root->left), stp2.push(root->right);
+        
+        //loop over the whole tree while the stacks aren't empty
+        while(!stp1.empty()){
+            //initialize two pointers, each on the top value in the stack
+            TreeNode* p1=stp1.top(), *p2=stp2.top();
+            //remove the top element from each stack
+            stp1.pop(), stp2.pop();
+            
+            //compare the two new pointers
+
+            //if there're no ones, no left or right nodes-> skip this case
+            if(!p1 && !p2) continue;
+
+            //if the two nodes are valid & have the same values
+            if(p1 && p2 && p1->val==p2->val){
+                //push left then right nodes in the first stack
+                stp1.push(p1->left);
+                stp1.push(p1->right);
+                //push right then left nodes in the second stack
+                stp2.push(p2->right);
+                stp2.push(p2->left);
+            }
+            
+            //if (!p1 || !p2)
+            else
+                return false;
+        }
+        
+        //after that, the two trees are symmetric around the center-> return true
+        return true;
+    }
+};
