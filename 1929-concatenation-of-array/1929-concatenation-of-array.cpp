dd42106f259b2d01d1f0c564aed47db7ee642762
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-08-28 12:00:33 +0200

    Time: 8 ms (89.02%), Space: 12.5 MB (87.76%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        
+        int n=nums.size();
+        //initialize a new array with a double size of the old one with initial values of zeros
+        vector<int> ans (n*2);
+        
+        //loop over the whole array elements
+        for(int i=0;i<n;i++){
+            //copy the old values to the new array in the same position
+            ans[i]=nums[i];
+            //copy the same old values to the new array but in the position after the end of the old ones
+            ans[i+n]= nums[i];
+        }
+        
+        //return the new array elements
+        return ans;
+    }
+};
+
