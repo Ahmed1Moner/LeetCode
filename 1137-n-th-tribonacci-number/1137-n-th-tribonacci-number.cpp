ea0e8c46526f111079f5ee760d8d0d8af48928d2
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-09-11 20:32:56 +0200

    Time: 0 ms (100.00%), Space: 5.9 MB (53.38%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,49 +1,51 @@
-//tabulation
+//memoization
 class Solution {
 public:
+    
+    //initialize an array of -1s
+    int dp[38]={-1};
+
     int tribonacci(int n) {
         
-        //initialize an array of -1s
-        int dp[38]={-1};
+        //handle invalid cases
+        if(n<=0)
+            return 0;
+        //base case 1: if n=1 | n=2-> 1
+        if(n==1 || n==2)
+            return 1;
+        //base case 2: if the position reached before-> return its result immediately
+        if(dp[n]!=0)
+            return dp[n];
         
-        //initial values of the base cases
-        dp[0]=0;dp[1]=1;dp[2]=1;
         
-        //loop over the whole array & adding the last three numbers of the given number
-        for(int i=3;i<=n;i++)
-            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
-        
-        //return the value of the last position reached in the array
-        return dp[n];
-        
+        //the current position will be the sum of the last three ones
+        return dp[n]=tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
     }
 };
 
-
-//memoization
+//tabulation
 // class Solution {
 // public:
-    
-//     //initialize an array of -1s
-//     int dp[38]={-1};
-
 //     int tribonacci(int n) {
         
-//         //handle invalid cases
-//         if(n<=0)
-//             return 0;
-//         //base case 1: if n=1 | n=2-> 1
-//         if(n==1 || n==2)
-//             return 1;
-//         //base case 2: if the position reached before-> return its result immediately
-//         if(dp[n]!=0)
-//             return dp[n];
+//         //initialize an array of -1s
+//         int dp[38]={-1};
         
+//         //initial values of the base cases
+//         dp[0]=0;dp[1]=1;dp[2]=1;
         
-//         //the current position will be the sum of the last three ones
-//         return dp[n]=tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+//         //loop over the whole array & adding the last three numbers of the given number
+//         for(int i=3;i<=n;i++)
+//             dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+        
+//         //return the value of the last position reached in the array
+//         return dp[n];
+        
 //     }
 // };
+
+
+
 
 //Recursion
 
