3ad5e63ee5e86eef3a8c03c2830126b541ac1c95
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-09-15 02:18:44 +0200

    Time: 534 ms (17.48%), Space: 84.3 MB (97.18%) - LeetHub

--- 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,17 +1,64 @@
 class Solution {
 public:
     
-      int minimizedMaximum(int n, vector<int>& A) {
-        int left = 1, right = 100000;
-        while (left < right) {
-            int mid = (left + right) / 2, sum = 0;
-            for (int a : A)
-                sum += (a + mid - 1) / mid;
-            if (sum > n)
-                left = mid + 1;
-            else
-                right = mid;
+    //helper function to check if the middle pointer value of the range is a valid value to be divided or not
+    bool valid(int mid, vector<int> &quantities, int n){
+        
+        //initialize a counter
+        int counter=0;
+
+        //loop over the whole array elements
+        for(int qu:quantities){
+            //check if the current element is smaller than the mid
+            if(qu<mid)
+                //if yes-> add the counter by one
+                counter++;
+            
+            else{
+                //if no->initialize a temp partial sum variable
+                int parSum=qu;
+                
+                //loop until getting 0 
+                while(parSum>0){
+                    //subtract the mid value from the current sum
+                    parSum-=mid;
+                    //add the counter by one
+                    counter++;
+                }
+            }
         }
-        return left;
+        
+        //check if the counted products is valid or not compared to the allowed ones
+        return counter<=n;
+    }
+    
+    
+    
+    int minimizedMaximum(int n, vector<int>& quantities) {
+        
+        //handle invalid cases
+        if(n<1)
+            return 0;
+  
+        //initialize mainly two pointers-> one on the minimum possible value in the array & one on the maximum one
+        int left=1, right=*max_element(quantities.begin(), quantities.end()), ans=-1;
+        
+        //loop while the left pointer is before the right one
+        while(left<right){
+            //the third pointer mid-> is exactly the middle element between the main two ones
+            int mid=left+(right-left)/2;
+        //call the helper function
+            if(valid(mid, quantities, n))
+                //case 1: if yes, valid-> move the right pointer to the mid position
+                right=mid;
+            
+            else
+                //case 2: if not-> move the left pointer to the position after the mid one
+                left=mid+1;
+                
+        }
+        
+        //return the right position 
+        return right;
     }
 };
