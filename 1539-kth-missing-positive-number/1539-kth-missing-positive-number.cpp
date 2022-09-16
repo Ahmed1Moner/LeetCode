b1258a4cec9e82652e5c6e3d4d6d57373c67566c
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-09-16 14:39:58 +0200

    Time: 11 ms (39.19%), Space: 9.4 MB (99.82%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -2,12 +2,44 @@ class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
         
-        //since the array elements are strictly incresing, we'll llop over the whole array elements & count the number of the elements which are smaller than the given kth missing integer & add them to it
+        //handle invalid cases
+        if(k<=0)
+            return 0;
         
-        for(int num:arr)
-            if(num<=k)
-                k++;
-        return k;
+        //initialize mainly two pointers-> one on the first element in the array & one on last one
+        int left=0, right=arr.size();
         
+        //loop while the left pointer is before the right one
+        while(left<right){
+            
+            //the third pointer mid-> is exactly the middle element between the main two ones
+            int mid=left+(right-left)/2;
+            
+            //check the number of the missing elements
+            if((arr[mid]-mid-1)<k)
+                //case 1: if still smaller than k-> move the left pointer to the position after the mid one
+                left=mid+1;
+            else
+                //case 2: if not, valid-> move the right pointer to the mid position
+                right=mid;
+        }
+        
+        //return the left position + the kth index
+        return left+k;
     }
 };
+
+
+// class Solution {
+// public:
+//     int findKthPositive(vector<int>& arr, int k) {
+        
+//         //since the array elements are strictly incresing, we'll llop over the whole array elements & count the number of the elements which are smaller than the given kth missing integer & add them to it
+        
+//         for(int num:arr)
+//             if(num<=k)
+//                 k++;
+//         return k;
+        
+//     }
+// };
