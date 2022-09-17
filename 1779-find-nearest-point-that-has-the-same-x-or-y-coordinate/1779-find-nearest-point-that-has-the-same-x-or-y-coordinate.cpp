55719c3141a00859287c7f9dd73c9947bdecfe75
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-09-17 21:28:02 +0200

    Time: 353 ms (20.02%), Space: 59.4 MB (73.79%) - LeetHub

--- 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.cpp
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.cpp
@@ -2,26 +2,24 @@ class Solution {
 public:
     int nearestValidPoint(int x, int y, vector<vector<int>>& points) {
         
-        //initialize a position & minimum possible absolute value & calculated distance variables
-        int minValue=INT_MAX, dis=0, index=-1;
+        //initialize a position & minimum possible absolute value & calculated  variables
+        int minValue=INT_MAX, index=-1;
         
         //loop over the whole array elements
-        for(int i=0;i<points.size();i++){
+        for(int i=0;i<points.size();i++)
             
             //check if the x-coordinates are equals or the y ones
-            if(points[i][0]==x || points[i][1]==y){
-                //calculate the manhattan distance between the two points
-                dis=abs(points[i][0]-x)+abs(points[i][1]-y);            
-                
-                //check if the current distance is smallest reached one
-                if(dis<minValue){
+            if(points[i][0]==x || points[i][1]==y)
+
+                //check if the current manhattan distance is smallest reached one
+                if((abs(points[i][0]-x)+abs(points[i][1]-y))<minValue){
                     //if no-> save the lowest value
-                    minValue=dis;
+                    minValue=abs(points[i][0]-x)+abs(points[i][1]-y);
                     //also, the current updated index
                     index=i;
                 }
-            }
-        }
+            
+        
         
         //return the index & if there's no one-> return -1 as it initialized before
         return index;
