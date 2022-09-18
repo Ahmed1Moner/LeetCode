7995511d56ad12872ff7eb8dded702fa81d5535a
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-09-18 21:13:12 +0200

    Time: 15 ms (31.42%), Space: 13.6 MB (81.26%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -3,12 +3,12 @@ public:
     int minCostClimbingStairs(vector<int>& cost) {
         
         //loop over the whole array elements
-        for(int i=2;i<cost.size();i++)
-            //compare the last two elements value & get the minimum of them & update the current cost with the sum of the result to it
-            cost[i]+=min(cost[i-1], cost[i-2]);
+        for(int i=cost.size()-3;i>=0;i--)
+            //compare the next two elements values & get the minimum of them & update the current cost with the sum of the result to it
+            cost[i]+=min(cost[i+1], cost[i+2]);
         
-        //return the minimum cost values of the last two elements
-        return min(cost[cost.size()-1], cost[cost.size()-2]);
+        //return the minimum cost value of the first two elements
+        return min(cost[0], cost[1]);
     }
     
 };
