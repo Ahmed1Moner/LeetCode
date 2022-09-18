0cb8c7c91e23895dc2f419dd241d18f400e1c8b7
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-09-18 19:56:45 +0200

    Time: 96 ms (28.24%), Space: 22.1 MB (93.01%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,11 +1,31 @@
 class Solution {
 public:
     
+    bool helper(vector<int>& nums, int threshold, int mid, int sum){
+                
+        //loop over the whole array element each time
+        for(int num:nums){
+            //check if the current element is divisble by the selected divisor
+            if(num%mid==0)
+                //if yes-> add the divisor value to the sum variable
+                sum+=(num/mid);
+            else
+                //if no-> add one to the result
+                sum+=(num/mid)+1;
+        
+//             OR use this formula, ceil division-> add the divisor value to the sum variable
+//                 sum+=(num+mid-1)/mid;
+        
+        }
+        
+        //return true if the final sum value is smaller than the given threshold
+        return sum>threshold;        
+    }
     
     int smallestDivisor(vector<int>& nums, int threshold) {
         
         //initialize two main pointer-> one on the lowest possible answer value & one on the largest
-        int left=1, right=1e6;
+        int left=1, right=*max_element(nums.begin(), nums.end());
         
         //loop while the left pointer is before the right one
         while(left<right){
@@ -13,23 +33,9 @@ public:
             //the third pointer mid-> is exactly the middle element between the main two ones
             int mid=left+(right-left)/2, sum=0;
             
-            //loop over the whole array element each time
-            for(int num:nums){
-                //check if the current element is divisble by the selected divisor
-                if(num%mid==0)
-                    //if yes-> add the divisor value to the sum variable
-                    sum+=(num/mid);
-                else
-                    //if no-> add one to the result
-                    sum+=(num/mid)+1;
-            }
             
-//             OR use this formula, ceil division-> add the divisor value to the sum variable
-//                 sum+=(num+mid-1)/mid;
-        
-        
             //then, check if the final sum is greater than the given threshold-> divisor is still small
-            if(sum>threshold)
+            if(helper(nums, threshold, mid, sum))
                 //case 1: if yes-> move the left pointer to the position after the mid one
                 left=mid+1;
             else
