03a809fb59a98a59742b87d38b48883b817260ef
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-09-18 23:17:08 +0200

    Time: 4 ms (50.00%), Space: 5.7 MB (100.00%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+        
+        //loop over the whole integer values
+        for(int i=1;i<=INT_MAX;i++)
+            //check first if the current value is divisible by 2
+            if(i%2==0)
+                //if yes-> then, check if it's divisible by the given n, too
+                if(i%n==0)
+                    //if yes-> return the valid i value
+                    return i;
+        
+        //if no, for both two conditions-> return 0
+        return 0;
+    }
+};
