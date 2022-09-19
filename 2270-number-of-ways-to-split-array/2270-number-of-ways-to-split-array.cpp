e56f77a22fa81cd7c7ff3d1ba05fc18f0dd7e801
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-09-19 13:41:41 +0200

    Time: 186 ms (89.02%), Space: 85.5 MB (89.77%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cpp
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cpp
@@ -3,11 +3,11 @@ public:
     int waysToSplitArray(vector<int>& nums) {
         
         //initialize two sum variables-> one from the left or start & one from the right or end
-        long long counter=0, sumLeft=0, sumRight=accumulate(nums.begin(), nums.end(), 0LL);
+        long long counter=0, sumLeft=0, sumRight=0;
         
         //btw, you can use this loop to get the sumRight value instead
-//         for(int num:nums)
-//             sumRight+=num;
+        for(int num:nums)
+            sumRight+=num;
         
         //loop over the whole array elements except the last one, as the element must have a right one 
         for(int i=0;i<nums.size()-1;i++){
