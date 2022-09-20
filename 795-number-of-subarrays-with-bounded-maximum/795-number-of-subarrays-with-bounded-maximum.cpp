f597e5169f60c9ad423e1ac1a6b4a62f5410ce20
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-09-20 18:28:47 +0200

    Time: 182 ms (9.80%), Space: 52.2 MB (68.61%) - LeetHub

---
+++ 795-number-of-subarrays-with-bounded-maximum/795-number-of-subarrays-with-bounded-maximum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
+        
+        //initialize a counter & two pointers for the boundaries
+        int counter=0, start=-1, end=-1;
+        
+        for(int i=0;i<nums.size();i++){
+            
+            //move the end pointer to the position of the next invalid number
+            if(nums[i]>right)
+                end=i;
+            //move the start pointer to the position of the first valid number
+            if(nums[i]>=left)
+                start=i;
+            
+            //subtract the two ones to get the distance, number of valid values, between the two poiters & add it to the counter
+            counter+=start-end;
+        }
+        
+        //return the final counter value
+        return counter;
+    }
+};
