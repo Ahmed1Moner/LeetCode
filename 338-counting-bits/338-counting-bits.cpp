4fae7e83cec84ce7e87418e47ff5c2718f498717
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-09-20 17:29:57 +0200

    Time: 7 ms (73.72%), Space: 7.8 MB (88.21%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,30 +1,45 @@
 class Solution {
 public:
-    
     vector<int> countBits(int n) {
         
         //initialize a new vector with (n+1) size with initial values of 0s
-        vector<int> ans(n+1,0);
+        vector<int> dp(n+1, 0);
         
-        //loop over the whole number, from 0 to n
-        for(int i=0;i<=n;i++){
+        //base case-> zero have no ones
+        dp[0]=0;
+        //initialize an offset variable to change its value with the pattern
+        int offset=1;
+        
+        /*
+        0->00(00)
+        1->00(01)
+        2->00(10)
+        3->00(11)
+        
+        4->01(00)
+        5->01(01)
+        6->01(10)
+        7->01(11)
+        
+        8->10(00)
+        9->10(01)
+        ...
+        each time, we're check the posiotion in the pattern & increasing the upcoming value with 1
+        */
+        
+        //loop over the whole number, from 1 to n
+        for(int i=1;i<=n;i++){
             
-            //initialize a counter & quotient & remainder variables
-            int counter=0, rem=0, que=i;
+            //check if the double offset value equals the current iterator one
+            if(2*offset==i)
+                //if yes-> replace the iterator value with the offset one
+                offset=i;
             
-            //loop until the queotient gets zero
-            while(que!=0){
-                
-                //update the remainder value by the modulus value 
-                rem+=(que%2);
-                //save the value in the new vector
-                ans[i]=rem;
-                //update the que value by dividing by 2
-                que/=2;
-            }
+            //store the value of the old four-values pattern +1 in the correct position
+            dp[i]=dp[i-offset]+1;
         }
         
-        //return the new vector values
-        return ans;
+        //return the new vector elements
+        return dp;
     }
 };
