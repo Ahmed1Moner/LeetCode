86720ab32b55eadebb7ab718f29d3630f1cac141
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-09-21 10:36:43 +0200

    Time: 229 ms (94.17%), Space: 132.7 MB (34.65%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -2,31 +2,37 @@ class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         
-        //initialize a new array
-        vector<int> res;
+        //initialize two new arrays
+        vector<int> small, large;
+        int counter=0;
         
         //loop over the whole array elements
-        for(int num:nums)
-            //check if the current value is smaller than the pivot
+        for(int num:nums){
+            
+            //case 1: the current value is smaller than the pivot
             if(num<pivot)
-                //append it in the new array
-                res.push_back(num);
+                //add it to the small numbers vector
+                small.push_back(num);
+            //case 2: the current value is greater than the pivot
+            else if(num>pivot)
+                //add it to the large numbers vector
+                large.push_back(num);
+            //case 3: the current value equals to the pivot
+            else
+                //add the counter by 1, to know how many pivots
+                counter++;
+        }
+      
+        //iterate with the number of the counter value
+        while((counter--)!=0)
+            //add the pivot value in the array
+            small.push_back(pivot);
         
-        //loop over the whole array elements, again
-        for(int num:nums)
-            //check if the current value equals to the pivot
-            if(num==pivot)
-                //append it in the new array
-                res.push_back(num);
+        //then, iterate the large array to move its elements to only one array, small
+        for(int num:large)
+            small.push_back(num);
         
-        //loop over the whole array elements, again
-        for(int num:nums)
-            //check if the current value is greater than the pivot
-            if(num>pivot)
-                //append it in the new array
-                res.push_back(num);
-        
-        //return the new array elements
-        return res;
+        //return the rearranged array numbers
+        return small;
     }
 };
