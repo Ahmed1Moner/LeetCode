3f36c9490476fe786b6cfb271e8786574e0f51d3
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-09-30 00:06:09 +0200

    Time: 5 ms (20.36%), Space: 6 MB (24.82%) - LeetHub

--- 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.cpp
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.cpp
@@ -2,30 +2,18 @@ class Solution {
 public:
     int countEven(int num) {
      
-        //initialize a counter
-        int counter=0;
+        int val=num, sum=0;
         
-        //loop over the whole numbers from 1 to the given number
-        for(int i=1;i<=num;i++){
-
-            //initialize a temporary sum 
-            int sum=0, val=i;
-            
-            //loop until the current number value gets 0
-            while(val!=0){
-                //add the first digit value to the counter
-                sum+=(val%10);
-                //update the num by removing the first digit
-                val/=10;
-            }
-            
-            //check if the final sum of each loop is even
-            if(sum%2==0)
-                //if yes-> add the coutner by one
-                counter++;
+        //loop until the current number value gets 0
+        while(num!=0){
+            //add the first digit value to the counter
+            sum+=(num%10);
+            //update the num by removing the first digit
+            num/=10;
         }
-        
-        //return the final counter value
-        return counter;
+
+        //following the sum of the numbers pattern & getting this formula
+        return (val-sum%2)/2;
+        //return sum%2==0 ? val/2 : (val-1)/2;
     }
 };
