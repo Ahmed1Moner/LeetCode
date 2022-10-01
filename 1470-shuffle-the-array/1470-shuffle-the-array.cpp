269d57d9e189657c0ca58f19db805ecb25b85fa1
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-10-01 23:45:19 +0200

    Time: 10 ms (52.96%), Space: 9.7 MB (85.33%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -2,15 +2,18 @@ class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
         
-        //initialize a new vector
-        vector<int> res;
+        //initialize a new vector with the same size of the given one
+        vector<int> res(nums.size());
         
-        //loop over the half array elements
-        for(int i=0;i<n;i++){
-            //append the current elements
-            res.push_back(nums[i]);
-            //then, append the current one in the second half array
-            res.push_back(nums[i+n]);
+        //initialize two pointers i&j
+        //loop over the half array elements with a pointer k
+        for(int i=0, j=n, k=0;i<n;){
+            //replace the current position with the first pointer value
+            res[k++]=nums[i++];
+            //replace the current position with the second pointer value
+            res[k++]=nums[j++];
+            
+            //incremeant the three pointers by one step
         }
         
         //return the new array elements
