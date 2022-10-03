b116c984f2b39c82dbf086d0523a06a4c3fc5d21
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-10-03 22:43:36 +0200

    Time: 3 ms (38.59%), Space: 5.9 MB (33.15%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.cpp
+++ 2427-number-of-common-factors/2427-number-of-common-factors.cpp
@@ -2,14 +2,19 @@ class Solution {
 public:
     int commonFactors(int a, int b) {
               
-        int counter=0, maxVal=max(a,b);
+        //initialize a counter
+        int counter=0;
         
-        for(int i=1;i<=maxVal;i++)
+        //loop from 1 until the maximum value between the two integers
+        for(int i=1;i<=max(a,b);i++)
+            
+            //check if the current element is divisible by the two given integers
             if(a%i==0)
                 if(b%i==0)
+                    //if yes-> add trhe counter by one
                     counter++;
         
-        
+        //return the final counter value
         return counter;
     }
 };
