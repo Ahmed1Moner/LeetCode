3a35a20a4ed2b224de5bf81301f89ade449848bd
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-10-03 22:44:40 +0200

    Time: 3 ms (38.59%), Space: 5.9 MB (73.85%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.cpp
+++ 2427-number-of-common-factors/2427-number-of-common-factors.cpp
@@ -5,14 +5,13 @@ public:
         //initialize a counter
         int counter=0;
         
-        //loop from 1 until the maximum value between the two integers
-        for(int i=1;i<=max(a,b);i++)
+        //loop from 1 until the minimum value between the two integers
+        for(int i=1;i<=min(a,b);i++)
             
             //check if the current element is divisible by the two given integers
-            if(a%i==0)
-                if(b%i==0)
-                    //if yes-> add trhe counter by one
-                    counter++;
+            if(a%i==0 && b%i==0)
+                //if yes-> add trhe counter by one
+                counter++;
         
         //return the final counter value
         return counter;
