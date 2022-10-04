f512c5e7df24c5794a0063a035199bddd89f240b
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-10-04 13:49:34 +0200

    Time: 6 ms (98.92%), Space: 21.9 MB (38.14%) - LeetHub

--- 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.cpp
+++ 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.cpp
@@ -11,43 +11,32 @@
  */
 class Solution {
 public:
-    
-    //initialize two variables, one to store the correct node value & one to compare nodes to get the max one
-    int value=0, max=INT_MIN;
-    
-    
     int findBottomLeftValue(TreeNode* root) {
         
-        //base case 1
-        if(!root)
-            return -1;
+        //initialize a queue & add the whole tree in it
+        queue<TreeNode*> q;
+        q.push(root);
         
-        //recursively, call the helper function with intial value of level counter=0
-        helper(root, 0);
-        
-        //return the final valid value
-        return value;
-    }
-    
-    void helper(TreeNode* node, int counter){
-         
-        //check if we're in the maximum deapth/level in the tree
-        if(counter>max){
-            //if yes-> save the max level until now
-            max=counter;
-            //save the current node value
-            value=node->val;
+        //loop until the queue gets empty
+        while(!q.empty()){
+            
+            //save the current queue element's value
+            root=q.front();
+            //remove the current element
+            q.pop();
+            
+            //if the current node have a right node
+            if(root->right)
+                //add it to the queue
+                q.push(root->right);
+            
+            //if the current node have a left node
+            if(root->left)
+                //add it to the queue
+                q.push(root->left);
         }
         
-        //if the current node have a left node
-        if(node->left)
-            //recursively, call the helper function with its left nodes, add the level counter by one
-            helper(node->left, counter+1);
-        
-        //if the current node have a right node
-        if(node->right)
-            //recursively, call the helper function with its right nodes, add the level counter by one
-            helper(node->right, counter+1);
+        //after that, i just need the last node value in the whole tree
+        return root->val;
     }
-    
 };
