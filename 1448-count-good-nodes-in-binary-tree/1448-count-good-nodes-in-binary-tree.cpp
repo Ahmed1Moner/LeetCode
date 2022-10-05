9bbac4428c66c7d4ce43c3196be9b6186b5ec856
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-10-05 10:23:33 +0200

    Time: 218 ms (57.08%), Space: 90.1 MB (9.11%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -11,40 +11,47 @@
  */
 class Solution {
 public:
-    
-    //initialize a counter
-    int counter=0;
-    
+
     int goodNodes(TreeNode* root) {
         
         //base case 1
         if(!root)
             return 0;
         
-        //call the helper function 
-        helper(root, INT_MIN);
+        //initialize a queue of both tree & integer nodes as pair
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, INT_MIN});
+        int counter=0;
+        
+        //loop until the queue gets empty
+        while(!q.empty()){
+            
+            //save the current queue element's value
+            auto [root, maxVal]=q.front();
+          //remove the current element
+            q.pop();
+            
+            //base case 2
+            if(root->val>=maxVal){
+                //add the counter by one
+                counter++;
+                //update the current node value in the maximum variable
+                maxVal=root->val;
+            }
+            
+            //if the current node have a left node
+            if(root->left)
+                //add it to the queue
+                q.push({root->left, maxVal});
+            //if the current node have a right node
+            if(root->right)
+                //add it to the queue
+                q.push({root->right, maxVal});
+            
+        }
         
         //return the final counter value
         return counter;
     }
     
-    void helper(TreeNode* root, int maxVal){
-        
-        //base case 2
-        if(!root)
-            return;
-                
-        //base case 3
-        if(root->val>=maxVal){
-            //add the counter by one
-            counter++;
-            //update the current maximum value with the current node one
-            maxVal=root->val;
-        }       
-        
-        //recursively, call the helper function with the current maximum value with either the left or the right nodes
-        helper(root->left, maxVal);
-        helper(root->right, maxVal);        
-        
-    }
 };
