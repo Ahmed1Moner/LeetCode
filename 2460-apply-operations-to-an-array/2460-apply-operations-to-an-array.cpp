ca7e172c800d539faf42ed0b06f201e2679eb318
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-11-11 19:59:42 +0200

    Time: 12 ms (26.27%), Space: 8.9 MB (55.69%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
@@ -2,30 +2,23 @@ class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
         
-        
         //loop over the whole array elements
-        for(int i=0;i<nums.size()-1;i++){
+        for(int i=0,j=0;i<nums.size();i++){
             
-            //check if the current element equals to the next one
-            if(nums[i]==nums[i+1]){
+            //for this comparison only, check if it's a valid index first
+            if(i<nums.size()-1 && nums[i]==nums[i+1]){
                 
-                //if yes-> multiply it by 2 && set the next one to zero
+                //if the two consecutive elements are equal-> multiply the element by 2 && set the next one to zero
                 nums[i]*=2;
                 nums[i+1]=0;
             }
+            
+            //in the same loop, if the current isn't zero-> swap it by the second pointer's position
+            if(nums[i]!=0){
+                swap(nums[i], nums[j++]);
+            }
         }
         
-        int j=0;
-        
-        //loop again to shift the non-zero elements to be the first ones
-        for(int num:nums)
-            if(num!=0)
-                nums[j++]=num;
-        
-        //loop again to shift the zeros elements to be the last ones
-        while(j<nums.size())
-            nums[j++]=0;
-            
         //return the new array elements
         return nums;
     }
