b03cf3fb45f80649206d180008360fa06b6c6a3f
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-12-03 21:08:26 +0200

    Time: 12 ms (82.76%), Space: 7.3 MB (55.77%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,27 +2,12 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         
-        //handle if there've not the same size
-        if(s.size()!=t.size())
-            return false;
+        //sort the letters of the 1st string
+        sort(s.begin(), s.end());
+        //sort the letters of the 2nd string
+        sort(t.begin(), t.end());
         
-        //initialize two maps
-        unordered_map<char, int>m1, m2;
-        
-        //loop over the 1st string & incremeant its location in 1st map
-        for(char c:s)
-            m1[c]++;
-        
-        //loop over the 2nd string & incremeant its location in 2nd map
-        for(char c:t)
-            m2[c]++;
-        
-        //loop again to check the values of the same letter in the two maps
-        for(int i=0;i<s.size();i++)
-            if(m1[s[i]]!=m2[s[i]])
-                //if they're different-> false
-                return false;
-        
-        return true;
+        //return true if they're the same exactly
+        return s==t;
     }
 };
