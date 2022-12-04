a37ef3472fd7a76b2cc2ed971976f179633d6301
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-12-04 20:44:55 +0200

    Time: 37 ms (62.73%), Space: 19.6 MB (60.90%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        map<int, int> mp;
+        
+        for(int i=0; i<nums.size();i++){
+            
+            if(mp[nums[i]]>=nums.size()/2)
+                return nums[i];
+            mp[nums[i]]++;
+        }
+        
+        return 0;
+    }
+};
