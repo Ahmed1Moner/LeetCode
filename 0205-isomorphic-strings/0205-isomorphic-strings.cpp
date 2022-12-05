b1e02147ced618fba448b7cf20d8d2f2c5db5b93
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-12-05 14:34:27 +0200

    Time: 29 ms (10.82%), Space: 7.2 MB (18.38%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -5,13 +5,12 @@ public:
         if(s.size()!=t.size())
             return false;
         
-        map<char,char> mp1, mp2;
+        map<char, char> mp1, mp2;
         
         for(int i=0;i<s.size();i++){
             
-            if((mp1[s[i]] && mp1[s[i]]!=t[i]) || (mp2[t[i]] && mp2[t[i]]!=s[i])){
+            if((mp1[s[i]]&&mp1[s[i]]!=t[i]) || (mp2[t[i]] && mp2[t[i]]!=s[i]))
                 return false;
-            }
             
             mp1[s[i]]=t[i];
             mp2[t[i]]=s[i];
