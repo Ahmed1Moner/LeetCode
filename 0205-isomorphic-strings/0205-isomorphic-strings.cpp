830e22d7cc150778f184d084168868b3cbd16534
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-12-05 14:35:00 +0200

    Time: 23 ms (23.50%), Space: 7.2 MB (18.38%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -2,20 +2,26 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) {
         
+        //handle if they've different sizes
         if(s.size()!=t.size())
             return false;
         
-        map<char, char> mp1, mp2;
+        //initialize two maps
+        map<char,char> mp1, mp2;
         
+        //loop over the two strings in the same time
         for(int i=0;i<s.size();i++){
             
-            if((mp1[s[i]]&&mp1[s[i]]!=t[i]) || (mp2[t[i]] && mp2[t[i]]!=s[i]))
+            //
+            if((mp1[s[i]] && mp1[s[i]]!=t[i]) || (mp2[t[i]] && mp2[t[i]]!=s[i]))
                 return false;
             
+            //save the current letter 
             mp1[s[i]]=t[i];
             mp2[t[i]]=s[i];
         }
         
+        //after all, if we pass these tests successfully-> true
         return true;
     }
 };
