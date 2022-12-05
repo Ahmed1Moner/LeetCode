8d34da01151c840a5760865fca7473eb0f0e5f89
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-12-05 14:27:31 +0200

    Time: 31 ms (9.13%), Space: 7.1 MB (18.38%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        
+        if(s.size()!=t.size())
+            return false;
+        
+        map<char,char> mp1, mp2;
+        
+        for(int i=0;i<s.size();i++){
+            
+            if((mp1[s[i]] && mp1[s[i]]!=t[i]) || (mp2[t[i]] && mp2[t[i]]!=s[i])){
+                return false;
+            }
+            
+            mp1[s[i]]=t[i];
+            mp2[t[i]]=s[i];
+        }
+        
+        return true;
+    }
+};
