1a9ba7f711d9dac4cc2525506db81a15662d7eb9
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-12-12 23:48:41 +0200

    Time: 1751 ms (16.84%), Space: 194.6 MB (35.30%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -5,33 +5,34 @@ public:
     //build the graph
         
         //build the adjacency list of the given vector elements
-        vector<vector<int>> adj(n);
-        for(int i=0;i<edges.size();i++){
-            adj[edges[i][1]].push_back(edges[i][0]);
-            adj[edges[i][0]].push_back(edges[i][1]);
+        unordered_map<int,vector<int>> adj; 
+        for(auto e : edges) {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
         }
         
     //BFS
         
         //initialize a vector for visited elements
         vector<int> visited(n, false);
-        //a queue for BFS
-        queue<int>q;
+        //a stack for BFS
+        stack<int>st;
         //start at the source point
-        q.push(source);
+        st.push(source);
         //mark it as visited
         visited[source]=true;
         
-        //loop while the queue still having elements
-        while(!q.empty()){
+        //loop while the stack still having elements
+        while(!st.empty()){
             
             //save the current top element in the queue & remove it
-            int top=q.front();
-            q.pop();
-            
+            int top=st.top();
+            st.pop();
+
             //check if we found the destination-> path exists
             if(top==destination)
                 return true;
+            
             
             //loop over the whole neighbour elements of the current element in the graph
             for(auto item:adj[top]){
@@ -39,13 +40,13 @@ public:
                 //if it's not visited yet
                 if(!visited[item]){
                     //push it in the queue & mark it as visited
-                    q.push(item);
+                    st.push(item);
                     visited[item]=true;
                 }
-            }
+            }            
         }
         
-        //after all, if there's no path exists-> false
         return false;
+        
     }
 };
