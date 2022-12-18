d73cbdac59352dbfe039d90da163ac6478e96c52
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-12-18 17:16:17 +0200

    Time: 24 ms (85.59%), Space: 6 MB (14.53%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -2,11 +2,21 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
         
-        //base case
+        //base case 1
         if(n<=0)
             return false;
         
-        //true if the number is divisable by 2 & the last reached value =1
-        return ((n==1)||(isPowerOfThree(n/3)==1 && (n%3==0)));
+        //base case 2
+        if(n==1)
+            return true;
+            
+         //loop & divide the value by 3 while the result is integer
+        while(n%3==0)
+            
+            //recursivelly, call the updated n value
+            return isPowerOfThree(n/3);
+        
+        //after all, if it's not valid, not integer or not divisible by 3-> false
+        return false;
     }
 };
