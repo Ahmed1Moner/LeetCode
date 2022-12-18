09925a09abbc546eecac0cf8d63714f2a4d5ed84
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-12-18 16:57:30 +0200

    Time: 0 ms (100.00%), Space: 6 MB (32.49%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -3,10 +3,10 @@ public:
     bool isPowerOfTwo(int n) {
         
         //base case
-        if(n==0)
+        if(n<=0)
             return false;
         
-        //true if the number is divisable by 2 & the last reached value =1
-        return ((n==1)||(isPowerOfTwo(n/2)==1 && (n%2==0)));
+        //true if the value of log base 2 (number) is integer
+        return (log2(n) == floor(log2(n)));
     }
 };
