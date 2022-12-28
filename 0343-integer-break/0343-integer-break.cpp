db46097bec20cd13beee451089c582bcc57efb95
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2022-12-28 22:38:11 +0200

    Time: 3 ms (47.04%), Space: 5.8 MB (92.83%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
+
+    //initialize a fixed-size array
+    int dp[60];
     
-    int helper(int n, int i){
+    int helper(int n){
         
-        //base case 1, if we reaches 1-> return the maximum counter value
-        if(n == 0) return 1;
-        //base case 2, if we gets a negative number-> break
-        if(n < 0 || i <= 0) return 0;
+        //base case 1
+        if(n<=2)
+            return 1;
         
-        //return the maximum value of calling the helper functions
-        //if 4-> (3,1), (2,2), (1,3) 
-        //by default, decremeant i by 1 & multiply the current i*n-i
-        return max(helper(n, i-1), i*helper(n-i, i));
-               
+        //base case 2, if the element calculated before-> return it
+        if(dp[n]!=-1)
+            return dp[n];
+        
+        
+        int res=INT_MIN;
+        
+        //loop over the whole numbers until the given one
+        for(int i=1;i<n;i++){
+            
+            //return the maximum value of calling the helper functions
+            //if 4-> (3,1), (2,2), (1,3) 
+            res=max(res, max(i*helper(n-i), i*(n-i)));
+        
+        }
+        
+        //save the fresh-calculated number's value
+        return dp[n]=res;
     }
     
     int integerBreak(int n) {
         
-        //call the helper function
-        return helper(n, n-1);
+        //fill all the elements of the vector with -1s
+        memset(dp, -1, sizeof(dp));
+        
+        //call the helper function & return the maximum reached counter value
+        return helper(n);
     }
 };
