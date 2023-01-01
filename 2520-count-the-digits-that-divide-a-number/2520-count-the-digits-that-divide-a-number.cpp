670cfb12f5387bdc090b4f619a148ac06682eaec
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2023-01-01 23:37:37 +0200

    Time: 3 ms (40.00%), Space: 6 MB (13.33%) - LeetHub

--- 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.cpp
+++ 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.cpp
@@ -4,22 +4,12 @@ public:
         
         //initialize a counter & save the given number
         int counter=0;
-        int temp=num;
-        int temp2=0;
-        
+     
         //loop over the whole number's digit
-        while(num!=0){
-            
-            //save the current digit of the number
-            temp2=num%10;
-            //check if the given number is divisable by the saved current digit
-            if(temp%temp2==0)
-                //if yes-> add the counter by 1
-                counter++;
-            
-            //update the number's value by removing a digit
-            num/=10;
-        }
+        for(int i=num;i!=0;i/=10)
+            //check if the given number is divisable by the current digit
+            //if yes-> add the counter by 1
+            counter+=num%(i%10)==0;
         
         //return the final counter value
         return counter;
