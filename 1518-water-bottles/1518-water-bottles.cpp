e72e7d7a0151e951d00abc58288ba9f49d6a15dd
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2023-01-08 16:16:11 +0200

    Time: 3 ms (34.85%), Space: 5.8 MB (83.67%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.cpp
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -5,14 +5,20 @@ public:
         int total=numBottles;
         int remainder=0;
         
+        //loop untile the exchange value become less than the bottles
         while(numBottles>=numExchange){
+            
+            //save the remainder bottles
             remainder=numBottles%numExchange;
+            //add the number of bottles that can be exchanged to the total
             total+=numBottles/numExchange;
+            //update the number of bottles
             numBottles/=numExchange;
+            //add the remainder to them
             numBottles+=remainder;
         }
         
-        
+        //return the final total value
         return total;
     }
 };
