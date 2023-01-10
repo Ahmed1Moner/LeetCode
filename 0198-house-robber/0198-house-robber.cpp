c1f96c6f522a989a5faed05712d8be5fd2873b6b
Ahmed Moner <95030742+Ahmed1Moner@users.noreply.github.com>
2023-01-11 01:03:35 +0200

    Time: 0 ms (100.00%), Space: 7.6 MB (79.94%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
     
-    int helper(vector<int> &nums, vector<int> &dp, int i){
-        
-        //base case: if the index reached the end of the vector-> return 
-        if(i>nums.size()-1)
-            return 0;
-        
-        //if the cuurent value is calculated before-> just return it
-        if(dp[i]!=-1)
-            return dp[i];
-        
-        //recursive case: get the maximum value between the two scenarios-> (if getting the current element + the next next ones) && (start getting from the next one) && save the calculated value in the dp vector
-        return dp[i]=max((nums[i]+helper(nums, dp, i+2)),(helper(nums, dp, i+1)));
-        
-    }
-    
+       
     int rob(vector<int>& nums) {
         
         //check first
         if(nums.size()==1)
             return nums[0];
         
-        //initialize a vector of -1s
-        vector<int> dp(nums.size()+1, -1);
+        int prev1=0;
+        int prev2=0;
         
-        //call the helper function
-        return helper(nums, dp, 0);
+        //loop over the whole vector elements
+        for(auto num:nums){
+            
+            //save the first previous element's value
+            int temp=prev1;
+            //get the maximum value between the two scenarios-> (if getting the previous previous value + the current one)
+            prev1=max(prev2+num, prev1);
+            //swap the values
+            prev2=temp;
+        }
+        
+        //return the last first previous element's value
+        return prev1;
         
     }
 };
